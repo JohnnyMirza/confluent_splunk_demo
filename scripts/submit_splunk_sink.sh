a40491a3daa067c7a52b6f2aee8855497fb2f391
JohnnyMirza <johnny.mirza@gmail.com>
2021-07-27 13:22:54 +1000

    update readme

--- scripts/submit_splunk_sink.sh
+++ scripts/submit_splunk_sink.sh
@@ -10,7 +10,6 @@ DATA=$( cat << EOF
     "splunk.hec.uri":"https://splunk_search:8089",
     "splunk.hec.token":"3bca5f4c-1eff-4eee-9113-ea94c284478a",
     "value.converter":"org.apache.kafka.connect.storage.StringConverter",
-    "
     "confluent.topic.bootstrap.servers":"broker:29092",
     "splunk.hec.json.event.formatted": true,
     "tasks.max": "1"
