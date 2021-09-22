75fe702d562d8efd4b72b6ad3ab2a164726e71d4
JohnnyMirza <johnny.mirza@gmail.com>
2021-09-22 22:53:10 +1000

    added additional splunk hec token and sink connector

--- scripts/startKafkaConnectComponents.sh
+++ scripts/startKafkaConnectComponents.sh
@@ -34,15 +34,15 @@ EOF
 
 curl -X POST -H "${HEADER}" --data "${DATA}" http://localhost:8083/connectors
 
-echo "Starting the Spluk sink connector"
+echo "Starting the Splunk Sink connector - HEC Formatted"
 
 HEADER="Content-Type: application/json"
 DATA=$( cat << EOF
 {
-  "name": "SPLUNKSINK",
+  "name": "SPLUNKSINK_HEC",
   "config": {
     "confluent.topic.bootstrap.servers": "broker:29092",
-    "name": "SPLUNKSINK",
+    "name": "SPLUNKSINK_HEC",
     "connector.class": "com.splunk.kafka.connect.SplunkSinkConnector",
     "tasks.max": "1",
     "key.converter": "org.apache.kafka.connect.storage.StringConverter",
@@ -52,6 +52,31 @@ DATA=$( cat << EOF
     "splunk.hec.uri": "https://splunk_search:8088",
     "splunk.hec.ssl.validate.certs": "false",
     "splunk.hec.json.event.formatted": "true"
+  }
+}
+EOF
+)
+
+curl -X POST -H "${HEADER}" --data "${DATA}" http://localhost:8083/connectors
+
+echo "Starting the Splunk Sink connector - Raw"
+
+HEADER="Content-Type: application/json"
+DATA=$( cat << EOF
+{
+  "name": "SPLUNKSINK_RAW",
+  "config": {
+    "confluent.topic.bootstrap.servers": "broker:29092",
+    "name": "SPLUNKSINK_RAW",
+    "connector.class": "com.splunk.kafka.connect.SplunkSinkConnector",
+    "tasks.max": "1",
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "value.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "topics": "AGGREGATOR",
+    "splunk.hec.token": "3bca5f4c-1eff-4eee-9113-ea94c284478b",
+    "splunk.hec.uri": "https://splunk_search:8088",
+    "splunk.hec.ssl.validate.certs": "false",
+    "splunk.hec.json.event.formatted": "false"
   }
 }
 EOF
